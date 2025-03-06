534378cdd6a64948ae82fa699b73f5535a902e6a
Invaern <invaern@fastmail.com>
2025-03-06 20:16:47 +0100

    disabled unused providers

--- nvim/lua/config/opts.lua
+++ nvim/lua/config/opts.lua
@@ -16,9 +16,17 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
 -- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+-- Schedule the setting after `UiEnter` because it can increase startup-time.
+-- Remove this option if you want your OS clipboard to remain independent.
+-- See `:help 'clipboard'`
 vim.schedule(function()
 	vim.opt.clipboard = "unnamedplus"
 end)
+
+-- Turn off unnecessary providers (they show as warnings in :checkhealth)
+-- These providers are used for plugins written in their respective languages
+-- See `:help providers` for details
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_ruby_provider = 0
