07cb263a23276513365dd58b169836b4be5893db
Invaern <invaern@fastmail.com>
2026-04-30 17:20:03 +0200

    nvim: bump dependencies

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -1,7 +1,10 @@
+-- luacheck: globals vim
+
 return {
-	"ggandor/leap.nvim",
 	lazy = false,
+	url = "https://codeberg.org/andyg/leap.nvim",
 	config = function()
-		require("leap").set_default_mappings()
+		vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+		vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
 	end,
 }
